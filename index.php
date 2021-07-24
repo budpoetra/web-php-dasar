5a83ff78dfcf4a7da752fc3b602f6c754871e434
budpoetra <budpoetra@gmail.com>
2021-07-24 20:45:22 +0700

    Menambahkan fitur Pagination pada web : Finished

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require "functions.php";
 
 // Pengecekan Log In
 if ( !isset($_SESSION["login"]) ) {
@@ -7,9 +8,15 @@ if ( !isset($_SESSION["login"]) ) {
     exit;
 }
 
-require "functions.php";
+// Pagination
+// // Konfigurasi
+$jumlahDataPerhalaman = 2;
+$jumlahData = count(query("SELECT * FROM mahasiswa"));
+$jumlahHalaman = ceil($jumlahData / $jumlahDataPerhalaman);
+$halamanAktif = ( isset($_GET["page"]) ) ? $_GET["page"] : 1;
+$awalData = ( $jumlahDataPerhalaman * $halamanAktif ) - $jumlahDataPerhalaman;
 
-$mahasiswa = query("SELECT * FROM mahasiswa ORDER BY nama ASC");
+$mahasiswa = query("SELECT * FROM mahasiswa ORDER BY nama ASC LIMIT $awalData, $jumlahDataPerhalaman");
 
 // Fitur Searching
 $keyword = $_POST["keyword"];
@@ -43,6 +50,26 @@ if ( isset($_POST["search"]) ) {
     <input type="text" name="keyword" placeholder="Searching..." autocomplete="off" autofocus>
     <button type="submit" name="search">Search</button>
 </form>
+<br>
+
+<!-- Navigasi Page -->
+<div class="pagination" style="text-align: center;">
+    <?php if ( $halamanAktif > 1 ) : ?>
+        <a href="?page=<?= $halamanAktif - 1; ?>">&laquo;</a>
+    <?php endif; ?>
+
+    <?php for( $i = 1; $i <= $jumlahHalaman; $i++ ) : ?>
+        <?php if ( $i == $halamanAktif ) : ?>
+            <a href="?page=<?= $i; ?>" style="font-weight: bold; color: red;"><?= $i; ?></a>
+        <?php else : ?>
+            <a href="?page=<?= $i; ?>"><?= $i; ?></a>
+        <?php endif; ?>
+    <?php endfor; ?>
+
+    <?php if ( $halamanAktif < $jumlahHalaman ) : ?>
+        <a href="?page=<?= $halamanAktif + 1; ?>">&raquo;</a>
+    <?php endif; ?>
+</div>
 <br>
 
 <table border="1" cellspacing="0" cellpadding="10">
