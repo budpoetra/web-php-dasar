5946845a371e9a1d13ebc89d5f308f95afb431b9
budpoetra <86049759+budpoetra@users.noreply.github.com>
2021-07-25 21:38:40 +0700

    mengganti file index.php

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+
+// Pengecekan Log In
+if ( !isset($_SESSION["login"]) ) {
+    header("Location: login.php");
+    exit;
+}
+
+require "functions.php";
+
+$mahasiswa = query("SELECT * FROM mahasiswa ORDER BY nama ASC");
+
+// Fitur Searching
+$keyword = $_POST["keyword"];
+if ( isset($_POST["search"]) ) {
+    $mahasiswa = search($keyword);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB PHP DASAR</title>
+</head>
+<body>
+
+<h1>Web PHP Dasar</h1>
+
+<a href="add.php">Add New Data</a>
+<br>
+<br>
+
+<a href="logout.php">Log Out</a>
+<br>
+<br>
+
+<!-- Serching -->
+<form action="" method="POST">
+    <input type="text" name="keyword" placeholder="Searching..." autocomplete="off" autofocus id="search">
+</form>
+<br>
+
+<div id="table">
+    <table border="1" cellspacing="0" cellpadding="10">
+        <tr>
+            <th></th>
+            <th>No.</th>
+            <th>Photo</th>
+            <th>Nama</th>
+            <th>NPM</th>
+            <th>Jurusan</th>
+            <th>Email</th>
+        </tr>
+
+        <!-- DATA -->
+        <?php $i = 1; ?>
+        <?php foreach( $mahasiswa as $mhs ) : ?>
+            <tr>
+                <td>
+                    <a href="edit.php?id=<?= $mhs["id"]; ?>">Edit</a>
+                    |
+                    <a href="delete.php?nama=<?= $mhs["nama"]; ?>" onclick="return confirm('Apakah Anda Yakin Akan Menghapus Data Ini?');">Delete</a>
+                </td>
+                <td> <?= $i; ?> </td>
+                <td><img src="img/<?= $mhs["gambar"]; ?>" width="50"></td>
+                <td><?= $mhs["nama"] ?></td>
+                <td><?= $mhs["npm"] ?></td>
+                <td><?= $mhs["jurusan"] ?></td>
+                <td><?= $mhs["email"] ?></td>
+            </tr>
+            <?php $i++; ?>
+        <?php endforeach; ?>
+
+    </table> 
+</div>
+<script src="js/script.js"></script>
+</body>
+</html>
